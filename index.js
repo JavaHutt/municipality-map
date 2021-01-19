e1352fd5d577fc0285bc8782d9f0a6ae32db38b6
JavaHutt <alxmer111@gmail.com>
2021-01-19 22:42:18 +0400

    include leaflet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
+const path = require('path');
 const homeRoute = require('./route/home');
 
 const port = 3000;
@@ -13,6 +14,8 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'view');
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use('/', homeRoute);
 
